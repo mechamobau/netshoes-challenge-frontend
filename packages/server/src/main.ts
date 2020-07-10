2ffe1a34ef503801457224026cc14bf974626d31
Lucas Viana <vianadossantoslucas@gmail.com>
2020-07-10 10:43:02 -0300

    :heavy_plus_sign: adds cors

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import cors from 'cors';
+
 import productRoutes from './entities/product/product.routes';
 
 import bodyParser from 'body-parser';
@@ -10,6 +12,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use('/static', express.static(path.join(__dirname, 'assets')));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/product', productRoutes);
