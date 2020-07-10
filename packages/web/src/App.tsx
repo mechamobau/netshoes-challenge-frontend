9853f112684742b0313fb537ee748e522f626713
Lucas Viana <vianadossantoslucas@gmail.com>
2020-07-10 02:16:52 -0300

    :contruction: adds home page with mock

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/Router';
+import { CartProvider } from './hooks/useCart';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  
+  body {
+    font-family: 'Open Sans', Arial, Helvetica, sans-serif;
+  }
+`;
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <GlobalStyle />
+        <AppRouter />
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
--- packages/web/src/pages/Home/Home.tsx
+++ packages/web/src/pages/Home/Home.tsx
@@ -1,7 +1,75 @@
 import React from 'react';
+import PublicLayout from '../../components/templates/PublicLayout/PublicLayout';
+import ProductItem from '../../components/molecules/ProductItem/ProductItem';
+
+import styled from 'styled-components';
+
+const Grid = styled.div`
+  width: 25%;
+  display: inline-block;
+`;
 
 const Home = () => {
-  return <p>home works</p>;
+  return (
+    <PublicLayout>
+      <Grid>
+        <ProductItem
+          title="Camisa Nike Corinthians I"
+          currencyFormat="R$"
+          price={229.9}
+        />
+      </Grid>
+      <Grid>
+        <ProductItem
+          title="Camisa Nike Corinthians I"
+          currencyFormat="R$"
+          price={229.9}
+        />
+      </Grid>
+      <Grid>
+        <ProductItem
+          title="Camisa Nike Corinthians I"
+          currencyFormat="R$"
+          price={229.9}
+        />
+      </Grid>
+      <Grid>
+        <ProductItem
+          title="Camisa Nike Corinthians I"
+          currencyFormat="R$"
+          price={229.9}
+        />
+      </Grid>
+      <Grid>
+        <ProductItem
+          title="Camisa Nike Corinthians I"
+          currencyFormat="R$"
+          price={229.9}
+        />
+      </Grid>
+      <Grid>
+        <ProductItem
+          title="Camisa Nike Corinthians I"
+          currencyFormat="R$"
+          price={229.9}
+        />
+      </Grid>
+      <Grid>
+        <ProductItem
+          title="Camisa Nike Corinthians I"
+          currencyFormat="R$"
+          price={229.9}
+        />
+      </Grid>
+      <Grid>
+        <ProductItem
+          title="Camisa Nike Corinthians I"
+          currencyFormat="R$"
+          price={229.9}
+        />
+      </Grid>
+    </PublicLayout>
+  );
 };
 
 export default Home;
